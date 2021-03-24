901db0c07f0ce95b1d7994ec408e2c3de4a83639
yuchen820304 <60634787+yuchen820304@users.noreply.github.com>
2021-03-25 01:47:31 +0800

    0325RWD初步完成

---
+++ js/buy_select.js
@@ -0,0 +1,21 @@
+let check = document.querySelectorAll('.box');
+check.forEach(function (item, i) {
+  check[i].addEventListener('click', function () {
+    if (check[i].classList.contains('checked')) {
+      check[i].classList.remove('checked');
+    } else {
+      check[i].classList.add('checked');
+    }
+  });
+});
+
+let count = document.querySelectorAll('.count');
+count.forEach(function (item, i) {
+  count[i].addEventListener('click', function () {
+    if (count[i].classList.contains('checked')) {
+      count[i].classList.remove('checked');
+    } else {
+      count[i].classList.add('checked');
+    }
+  });
+});
