a20803cfcf770eb344f786aeeaf68b9e8621e442
FA00251\Kevin Chen <yuchen820304@gmail.com>
2022-02-18 18:44:29 +0800

    共用CSS檔拆出&首頁RWD圖片尺寸修正 漢堡選單點及空白處關閉

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,10 @@
 // 漢堡選單控制
 let btn = document.getElementsByClassName('menu_btn')[0];
 let hamburger = document.getElementsByClassName('hamburger')[0];
-btn.addEventListener('click', function () {
+
+btn.addEventListener('click', function (e) {
   if (hamburger.style.transform == 'translateX(0%)') {
+    window.event ? (window.event.cancelBubble = true) : e.stopPropagation();
     hamburger.style.transform = 'translateX(-100%)';
     hamburger.style.transition = '.3s'; //若在CSS設定transition會造成重整時選單會依動畫設定秒數重跑到初始位置，形成瞬間出現選單又消失的情況
   } else {
@@ -10,3 +12,12 @@ btn.addEventListener('click', function () {
     hamburger.style.transition = '.3s';
   }
 });
+
+// 點擊任何地方關閉漢堡選單
+document.addEventListener(
+  'click',
+  function () {
+    hamburger.style.transform = 'translateX(-100%)';
+  },
+  true,
+);
