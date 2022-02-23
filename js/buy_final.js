38403151bc5e4299824c3111d174c66398b499b6
FA00251\Kevin Chen <yuchen820304@gmail.com>
2022-02-23 15:50:09 +0800

    座位選擇優化

--- js/buy_final.js
+++ js/buy_final.js
@@ -1,8 +1,11 @@
 let seat = document.querySelectorAll('.my_seat');
-let checked = 0;
+// let checked = 0;
 let selected = document.getElementsByClassName('reset')[0];
 let pay = document.getElementsByClassName('payment')[0];
 let storage = localStorage;
+// let seatArr = []; //已選座位陣列
+let checkedCount = document.querySelectorAll('.checked'); //取得所有已選取的座位
+let seatArr = Array.prototype.slice.call(checkedCount); //querySelectorAll出來是類陣列無法使用JS陣列方法，因此轉成陣列
 
 let name = (document.getElementById('name').innerHTML =
   storage.getItem('name'));
@@ -28,25 +31,36 @@ seat.forEach(function (item, i) {
   seat[i].addEventListener('click', function () {
     if (seat[i].classList.contains('checked')) {
       seat[i].classList.remove('checked');
-      checked = checked - 1;
-    } else if (checked < count) {
+      seatArr.splice(0, 1);
+    } else if (seatArr.length < count) {
       seat[i].classList.add('checked');
-      checked = checked + 1;
-    } else if (count == 1) {
+      seatArr.push(seat[i]); //加入陣列
+    } else if (seatArr.length == 1) {
       seat.forEach(item => {
-        item.classList.remove('checked');
+        item.classList.remove('checked'); //一律全部清除選取樣式
+        seatArr.splice(0, seatArr.length); //陣列清空
       });
-      seat[i].classList.add('checked');
+      seat[i].classList.add('checked'); //該點選座位加入選取樣式
+      seatArr.push(seat[i]); //座位加入陣列
     } else {
-      alert('超出人數啦幹白癡哦');
+      if (seatArr.length == count) {
+        //選滿狀態繼續點選會從陣列第一個刪除，再加入新點選座位
+        seatArr[0].classList.remove('checked');
+        seatArr.splice(0, 1);
+      }
+      seat[i].classList.add('checked');
+      seatArr.push(seat[i]);
     }
   });
 });
 
 pay.addEventListener('click', function (e) {
   e.preventDefault();
-  if (checked < count) {
-    alert('幹還有' + (count - checked) + '位沒選啦87');
+  console.log(seatArr);
+  if (seatArr.length < count && seatArr.length > 0) {
+    alert('幹還有' + (count - seatArr.length) + '位沒選啦87');
+  } else if (seatArr.length == 0) {
+    alert('幹都沒選是要跟鬼看逆');
   }
 });
 
